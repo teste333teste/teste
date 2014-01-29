f75d49acf49a5d257713a818b0073a659b69b16f
lsitec93.vieira <felipe.vieira@lsitec.org.br>
2014-01-29 17:41:57 -0200

    teste

--- Hello.java
+++ Hello.java
@@ -1,11 +1,19 @@
-public class hello{
-	public static void main (string args []){
+public class Hello{
+	public static void main (String args []){
 		System.out.println ("Hello Bye!");
+		System.out.println ("Funciona!!");
+		System.out.println ("1234");
 	}
 }
-//Gustavo* s2 strokes
+
+//Gustavo* s2 strokes dasdasdas
 //rrrrrr testandooooo
 
+<<<<<<< .merge_file_DVHAKs
 
 // TESTANDO NO BRANCH
 //testeteste
+=======
+// ;)
+
+>>>>>>> .merge_file_FHpjls
