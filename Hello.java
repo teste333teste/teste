ec4bbc0d630f35db961d256e0087b6704de9a51a
lsitec91.garcon <antonio.garcon@lsitec.org.br>
2014-01-29 16:49:15 -0200

    b

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class hello{
-	public static void main (string args []){
+public class Hello{
+	public static void main (String args []){
 		System.out.println ("Hello Bye!");
 		System.out.println ("Funciona!!");
 	}
