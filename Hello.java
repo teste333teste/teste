ccc5ed7c11e3e0ab81a02469e03faf8f4a2ba645
lsitec91.garcon <antonio.garcon@lsitec.org.br>
2014-01-29 16:40:16 -0200

    boa

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
 public class hello{
 	public static void main (string args []){
 		System.out.println ("Hello Bye!");
+		System.out.println ("Funciona!!");
 	}
 }
 //Gustavo* s2 strokes
 //rrrrrr testandooooo
+// ;)
