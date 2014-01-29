16a204c1c6b94a6f782da48b01fa48b65583a0eb
Gustavo Andre <gustavo.souza@lsitec.org.br>
2014-01-29 16:45:59 -0200

    teste

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class hello{
 	public static void main (string args []){
 		System.out.println ("Hello Bye!");
 		System.out.println ("Funciona!!");
+		System.out.println ("123");
 	}
 }
 //Gustavo* s2 strokes
